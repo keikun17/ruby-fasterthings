d7a07b1a57f7a274297056cc17d3920497f27fe4
Cakey | Buddy Magsipoc <keikun17@gmail.com>
2013-05-04 17:27:16 +0800

    More heap tune-ups, shave startup time. But GC less often.

--- tune_malloc.sh
+++ tune_malloc.sh
@@ -1,8 +1,14 @@
-export RUBY_GC_MALLOC_LIMIT=90000000
+export RUBY_GC_MALLOC_LIMIT=1000000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1.25
+export RUBY_HEAP_MIN_SLOTS=800000
+export RUBY_FREE_MIN=600000
 
+
+# TODO : rename "/usr/lib/libtcmalloc_minimal.so.0.1.0" to "/usr/lib/libtcmalloc_minimal.so"
+#        so we can just go set LD_PRELOAD=/usr/lib/libtcmalloc_minimal.so
 if [ -f "/usr/lib/libtcmalloc_minimal.so.0.1.0" ]
 then
-  LD_PRELOAD=/usr/lib/libtcmalloc_minimal.so.0.1.0
+  export LD_PRELOAD=/usr/lib/libtcmalloc_minimal.so.0.1.0
 else
   echo "skipping LD_PRELOAD hack, libtcmalloc_minimal.so does not exist"
 fi
